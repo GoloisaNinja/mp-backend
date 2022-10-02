e9be956bfed5320a29f8fccd12070391aa6e87be
Golodhrim <holidayninjastaff@gmail.com>
2022-10-01 22:38:31 -0400

    removed monorepo fallback code

--- server.js
+++ server.js
@@ -31,12 +31,12 @@ app.use('/api/watchlist', watchlist);
 app.use('/api/favorite', favorite);
 app.use('/api/watched', watched);
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static('client/build'));
+// 	app.get('*', (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// 	});
+// }
 
 const PORT = process.env.PORT || 5000;
 
